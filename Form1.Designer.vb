2943c49d4d82f58a17785322f234dcf2310aa646
snackerh <whanlee4@naver.com>
2020-06-04 01:28:21 +0900

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.DataSet1 = New System.Data.DataSet()
+        Me.SSR = New System.Data.DataTable()
+        Me.DataColumn1 = New System.Data.DataColumn()
+        Me.DataColumn2 = New System.Data.DataColumn()
+        Me.DataColumn3 = New System.Data.DataColumn()
+        Me.Result = New System.Windows.Forms.ListView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PickupSelect = New System.Windows.Forms.CheckedListBox()
+        Me.onetime = New System.Windows.Forms.Button()
+        Me.tentimes = New System.Windows.Forms.Button()
+        Me.customtry = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SSR, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataSet1
+        '
+        Me.DataSet1.DataSetName = "NewDataSet"
+        Me.DataSet1.Tables.AddRange(New System.Data.DataTable() {Me.SSR})
+        '
+        'SSR
+        '
+        Me.SSR.Columns.AddRange(New System.Data.DataColumn() {Me.DataColumn1, Me.DataColumn2, Me.DataColumn3})
+        Me.SSR.MinimumCapacity = 500
+        Me.SSR.TableName = "SSR"
+        '
+        'DataColumn1
+        '
+        Me.DataColumn1.AllowDBNull = False
+        Me.DataColumn1.AutoIncrement = True
+        Me.DataColumn1.ColumnName = "No"
+        Me.DataColumn1.DataType = GetType(Integer)
+        '
+        'DataColumn2
+        '
+        Me.DataColumn2.AllowDBNull = False
+        Me.DataColumn2.ColumnName = "name"
+        '
+        'DataColumn3
+        '
+        Me.DataColumn3.ColumnName = "pickup"
+        Me.DataColumn3.DataType = GetType(Boolean)
+        '
+        'Result
+        '
+        Me.Result.Alignment = System.Windows.Forms.ListViewAlignment.[Default]
+        Me.Result.AutoArrange = False
+        Me.Result.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None
+        Me.Result.HideSelection = False
+        Me.Result.Location = New System.Drawing.Point(12, 12)
+        Me.Result.Name = "Result"
+        Me.Result.Size = New System.Drawing.Size(305, 435)
+        Me.Result.TabIndex = 0
+        Me.Result.UseCompatibleStateImageBehavior = False
+        Me.Result.View = System.Windows.Forms.View.Details
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.PickupSelect)
+        Me.GroupBox1.Location = New System.Drawing.Point(323, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(190, 130)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "가챠 선택"
+        '
+        'PickupSelect
+        '
+        Me.PickupSelect.FormattingEnabled = True
+        Me.PickupSelect.Location = New System.Drawing.Point(6, 20)
+        Me.PickupSelect.Name = "PickupSelect"
+        Me.PickupSelect.Size = New System.Drawing.Size(177, 100)
+        Me.PickupSelect.TabIndex = 2
+        '
+        'onetime
+        '
+        Me.onetime.Location = New System.Drawing.Point(329, 148)
+        Me.onetime.Name = "onetime"
+        Me.onetime.Size = New System.Drawing.Size(81, 26)
+        Me.onetime.TabIndex = 2
+        Me.onetime.Text = "단챠 시뮬"
+        Me.onetime.UseVisualStyleBackColor = True
+        '
+        'tentimes
+        '
+        Me.tentimes.Location = New System.Drawing.Point(416, 148)
+        Me.tentimes.Name = "tentimes"
+        Me.tentimes.Size = New System.Drawing.Size(90, 26)
+        Me.tentimes.TabIndex = 3
+        Me.tentimes.Text = "10연챠 시뮬"
+        Me.tentimes.UseVisualStyleBackColor = True
+        '
+        'customtry
+        '
+        Me.customtry.Location = New System.Drawing.Point(329, 180)
+        Me.customtry.Name = "customtry"
+        Me.customtry.Size = New System.Drawing.Size(177, 27)
+        Me.customtry.TabIndex = 4
+        Me.customtry.Text = "횟수 지정"
+        Me.customtry.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(324, 353)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(189, 94)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "현재 가챠 정보"
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("굴림", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label1.Location = New System.Drawing.Point(6, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(176, 74)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "타카네 한정," & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "엘레나 한정" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(~20.06.11)"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Button3)
+        Me.GroupBox3.Controls.Add(Me.Button2)
+        Me.GroupBox3.Controls.Add(Me.Button1)
+        Me.GroupBox3.Location = New System.Drawing.Point(323, 213)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(189, 134)
+        Me.GroupBox3.TabIndex = 6
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "저격 전용"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 72)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(177, 25)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "컴플할때까지(천장미고려)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(6, 103)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(177, 25)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "컴플할때까지(천장고려)"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(6, 22)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(177, 44)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "체크항목 저격 시뮬 시작"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(521, 459)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.customtry)
+        Me.Controls.Add(Me.tentimes)
+        Me.Controls.Add(Me.onetime)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Result)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SSR, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataSet1 As DataSet
+    Friend WithEvents SSR As DataTable
+    Friend WithEvents DataColumn1 As DataColumn
+    Friend WithEvents DataColumn2 As DataColumn
+    Friend WithEvents DataColumn3 As DataColumn
+    Friend WithEvents Result As ListView
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents PickupSelect As CheckedListBox
+    Friend WithEvents onetime As Button
+    Friend WithEvents tentimes As Button
+    Friend WithEvents customtry As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,258 @@
+﻿Imports System.IO
+Imports System.Net
+
+Public Class Form1
+
+    ' 통상 쓰알 105종
+    Public ssrList() As String = {
+        "[어서 오세요 우리의 극장에!] 아마미 하루카",
+        "[시작되는 장소] 카스가 미라이",
+        "[빛나는 별처럼] 모가미 시즈카",
+        "[모두의 시선을 독점] 이부키 츠바사",
+        "[꿈을 향해 내딛은 한걸음] 시라이시 츠무기",
+        "[음악으로 자아내는 세계] 사쿠라모리 카오리",
+        "[우아한 한 때] 시죠 타카네",
+        "[팬시 드리밍] 하코자키 세리카",
+        "[워터 드림] 미우라 아즈사",
+        "[청려고아한 아가씨] 에밀리 스튜어트",
+        "[반짝임에 몸을 맡기고] 미야오 미야",
+        "[전해지는 마음] 가나하 히비키",
+        "[샤이닝 월드] 줄리아",
+        "[웃음을 꽃피우는 마칭] 야부키 카나",
+        "[큐티 트릭♪] 미나세 이오리",
+        "[달달한 숲에 어서오세요♪] 키노시타 히나타",
+        "[눈부신 마드모아젤] 니카이도 치즈루",
+        "[가을을 장식하는 엔터테이너] 후타미 아미",
+        "[오늘도 기운 한 가득!] 나카타니 이쿠]",
+        "[리틀 레이스 퀸] 오오가미 타마키",
+        "[별이 내리는 성야] 하기와라 유키호",
+        "[언제나 여름인 파라다이스!] 마이하마 아유무",
+        "[드리미 플래닛] 타카야마 사요코",
+        "[신춘의 빛에 소원을 담아] 키사라기 치하야",
+        "[매혹의 엘레강트 타임] 시노미야 카렌",
+        "[베이스볼 걸] 나가요시 스바루",
+        "[신나는 스위트 타임] 후타미 마미",
+        "[겨울에도 기운 한 가득!] 코사카 우미",
+        "[상쾌한 바람에 이끌려] 키타카미 레이카",
+        "[깜찍한 여자 아이] 스오 모모코",
+        "[파워풀 걸] 후쿠다 노리코",
+        "[반짝이는 뮤지엄] 아키즈키 리츠코",
+        "[미소가 피는 계절] 시마바라 엘레나",
+        "[이노센트 가든] 텐쿠바시 토모카",
+        "[컬러풀 팩토리] 타카츠키 야요이",
+        "[로맨틱 랜드] 토쿠가와 마츠리",
+        "[해피☆파라!] 노노하라 아카네",
+        "[사랑스러운 시선] 호시이 미키",
+        "[로코나이즈 뮤직♪] 로코",
+        "[두근거림을 밟으며 가자♪] 요코야마 나오",
+        "[레이디 온 더 비치] 바바 코노미",
+        "[사명을 다하는 자] 모치즈키 안나",
+        "[트렌드를 선점♪] 토코로 메구미",
+        "[마음에 드는 이야기] 나나오 유리코",
+        "[선상의 테라피스트] 키타자와 시호",
+        "[톡톡 튀는 여름의 맛♪] 사타케 미나코",
+        "[초 비치발리볼 [킹]] 토요카와 후카",
+        "[초 비치발리볼 [선배]] 타카야마 사요코",
+        "[책의 세계에서 대모험!] 오오가미 타마키",
+        "[꿈빛식부] 스오 모모코",
+        "[세 자매 카페 [고양이]] 미야오 미야",
+        "[세 자매 카페 [손님]] 모모세 리오",
+        "[소악마 리빙 데드] 마츠다 아리사",
+        "[애니멀 퍼레이드♪] 키노시타 히나타",
+        "[극장 서스펜스 [탐정]] 시라이시 츠무기",
+        "[극장 서스펜스 [지배인]] 마카베 미즈키",
+        "[포춘 걸] 텐쿠바시 토모카",
+        "[동경의 대변신] 코사카 우미",
+        "[특별한 날을 위해] 로코",
+        "[해피 홀리 나이트] 키타카미 레이카",
+        "[환상의 빛 속에서] 니카이도 치즈루",
+        "[프로즌 크리스털] 미우라 아즈사",
+        "[뉴 이어 공] 후쿠다 노리코",
+        "[풀 스로틀★걸] 카스가 미라이",
+        "[스위트 팔레트 랜드] 나가요시 스바루",
+        "[흰 학처럼] 바바 코노미",
+        "[신나는♪ 히나마츠리] 후타미 아미",
+        "[헤븐리 플라워즈] 토쿠가와 마츠리",
+        "[블랙 스트랭스] 키쿠치 마코토",
+        "[더 챌린저!] 후타미 마미",
+        "[아늑한 차] 하기와라 유키호",
+        "[미라클 퍼포머] 마이하마 아유무",
+        "[두근거리는 순간] 시노미야 카렌",
+        "[푹신푹신 드리밍] 아마미 하루카",
+        "[아쿠아마린 머메이드] 키사라기 치하야",
+        "[우정의 메타모르포제] 나카타니 이쿠",
+        "[핫 서머 메모리즈] 가나하 히비키",
+        "[깜찍한 요괴 고양이 딸] 노노하라 아카네",
+        "[가을 향기에 휩싸여] 타나카 코토하",
+        "[스포츠☆스마일] 타카츠키 야요이",
+        "[동화 나라 이야기 [늑대]] 가나하 히비키",
+        "[동화 나라 이야기 [여행자]] 나가요시 스바루",
+        "[내가 좋아하는 것] 시죠 타카네",
+        "[그레이트 하비스트] 타카야마 사요코",
+        "[아웃사이더 [벨벳]] 키쿠치 마코토",
+        "[아웃사이더 [파이널 데이]] 아마미 하루카",
+        "[붉게 빛나며!] 요코야마 나오",
+        "[이 시대를 즐기며] 사쿠라모리 카오리",
+        "[성야를 장식하는 눈사람] 미야오 미야",
+        "[스포트라이트 너머로] 아키즈키 리츠코",
+        "[생각은 시가에 숨기고] 모가미 시즈카",
+        "[반격의 헌터] 이부키 츠바사",
+        "[주문] 야부키 카나",
+        "[…In The Name Of。 …LOVE?] 마카베 미즈키",
+        "[이머전시 이머전스] 로코",
+        "[Be My Boy] 모모세 리오",
+        "[오렌지색 하늘 아래] 토요카와 후카",
+        "[염소씨와 집배원] 에밀리 스튜어트",
+        "[VIVID 이매지네이션] 모치즈키 안나",
+        "[스마일 최고] 사타케 미나코",
+        "[외딴 섬 서스펜스 호러 [저택 주인]] 니카이도 치즈루",
+        "[외딴 섬 서스펜스 호러 [메이드]] 키타자와 시호",
+        "[가끔은 시소] 나카타니 이쿠",
+        "[프라이빗 로드쇼] 미나세 이오리",
+        "[칠흑의 저격수] 줄리아"
+    }
+
+    ' 페스 쓰알 23종
+    Dim fesList() As String = {
+        "[도시의 바람을 받으며] 시라이시 츠무기",
+        "[윈터 스노 매직] 사쿠라모리 카오리",
+        "[반짝반짝 콘체르토] 하코자키 세리카",
+        "[늠름하게, 화려하게] 에밀리 스튜어트",
+        "[순진한 레이니 데이즈] 카스가 미라이",
+        "[물방울 프리즘] 모가미 시즈카",
+        "[비 갠 뒤의 약속] 이부키 츠바사",
+        "[가을빛 하이, 터치♪] 타카츠키 야요이",
+        "[월광 판타지아] 미나세 이오리",
+        "[리얼 인텐션] 키타자와 시호",
+        "[인조이♡윈터 데이트] 야부키 카나",
+        "[스태리 글로우] 호시이 미키",
+        "[선샤인 브라이트] 가나하 히비키",
+        "[달처럼 빛나는] 시죠 타카네",
+        "[어제도 오늘도 그 다음에도…] 타나카 코토하",
+        "[베스트 샷 모멘트!] 토코로 메구미",
+        "[스마일 에브리데이♡] 시마바라 엘레나",
+        "[맛있게 드세요…♡] 스오 모모코",
+        "[쪽! 해버릴 거야♪] 나카타니 이쿠",
+        "[행복은 라지 사이즈로] 나나오 유리코",
+        "[크리스마스 미션] 모치즈키 안나",
+        "[연결되는 미소] 아마미 하루카",
+        "[그 앞의 빛에] 시노미야 카렌"
+    }
+
+    ' 쓰알 픽업
+    Dim ssrPickup() As String = {
+        "[엘리건트 링] 시죠 타카네",
+        "[축복에 둘러싸여] 시마바라 엘레나"
+    }
+
+    ' 스알 픽업
+    Dim srPickup() As String = {
+        "[수줍은 웃음의 나데시코] 에밀리 스튜어트",
+        "[교복 시리즈] 마이하마 아유무"
+        }
+
+    Dim allCardList As List(Of String) = New List(Of String)
+
+    Dim ssrChance As Double = 3.0
+    Dim srChance As Double = 12.0
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim header As New ColumnHeader()
+        header.Text = ""
+        header.Name = "col1"
+        header.Width = Result.Width
+        Result.Columns.Add(header)
+
+        For Each item As String In ssrPickup
+            PickupSelect.Items.Add(item)
+        Next
+        For Each item As String In srPickup
+            PickupSelect.Items.Add(item)
+        Next
+    End Sub
+
+    Private Sub NormalMode_CheckedChanged(sender As Object, e As EventArgs)
+        If allCardList.Count > 0 Then
+            allCardList.Clear()
+            PickupSelect.Items.Clear()
+        End If
+        For Each item As String In ssrList
+            allCardList.Add(item)
+            Result.Items.Add(item)
+        Next
+        For Each item As String In ssrPickup
+            allCardList.Add(item)
+            Result.Items.Add(item)
+            PickupSelect.Items.Add(item)
+        Next
+        For Each item As String In srPickup
+            Result.Items.Add(item)
+            PickupSelect.Items.Add(item)
+        Next
+    End Sub
+
+    Private Sub FesMode_CheckedChanged(sender As Object, e As EventArgs)
+        ssrChance = 6.0
+
+        If allCardList.Count > 0 Then
+            allCardList.Clear()
+            PickupSelect.Items.Clear()
+        End If
+        For Each item As String In ssrList
+            allCardList.Add(item)
+            Result.Items.Add(item)
+        Next
+        For Each item As String In fesList
+            allCardList.Add(item)
+            Result.Items.Add(item)
+        Next
+        For Each item As String In srPickup
+            Result.Items.Add(item)
+            PickupSelect.Items.Add(item)
+        Next
+    End Sub
+
+    Private Sub onetime_Click(sender As Object, e As EventArgs) Handles onetime.Click
+        gacha(1)
+    End Sub
+
+    Private Sub gacha(times As Integer)
+        If times <= 0 Then
+            MsgBox("0 이하의 숫자는 입력할 수 없습니다!")
+            Exit Sub
+        End If
+        Result.Items.Clear()
+
+        Dim rnd As Random = New Random
+        For index As Integer = 1 To times
+            Dim gacharesult As Double = rnd.Next(0, 10001) / 100
+
+            If gacharesult <= ssrChance Then
+                Result.Items.Add("SSR☆" + ssrList.ElementAt(Int(rnd.Next(0, ssrList.Length()))))
+            ElseIf gacharesult <= ssrChance + srChance Then
+                Result.Items.Add("금나비")
+            Else
+                Result.Items.Add("은나비")
+            End If
+
+        Next
+
+    End Sub
+
+    Private Sub tentimes_Click(sender As Object, e As EventArgs) Handles tentimes.Click
+        gacha(10)
+    End Sub
+
+    Private Sub customtry_Click(sender As Object, e As EventArgs) Handles customtry.Click
+        Dim trynum
+        trynum = InputBox("시행 횟수는?",, "10",,)
+        If IsNumeric(trynum) Then
+            gacha(Integer.Parse(trynum))
+        End If
+
+    End Sub
+End Class
